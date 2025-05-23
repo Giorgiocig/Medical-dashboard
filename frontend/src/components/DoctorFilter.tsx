84ac3b7b374e70b93308df742c9b32e162304a15
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-23 17:36:51 +0200

    Implement DoctorFilter component

---
+++ frontend/src/components/DoctorFilter.tsx
@@ -0,0 +1,14 @@
+import { Box, TextField, Typography } from '@mui/material'
+
+export default function DoctorFilter({ field }: { field: string }) {
+    return (
+        <Box>
+            <Typography>{field}</Typography>
+            <TextField
+                id={`filter-${field}`}
+                label={`filter-${field}`}
+                variant='outlined'
+            />
+        </Box>
+    )
+}
