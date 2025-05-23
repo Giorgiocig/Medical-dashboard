89d3f86161ec42b613e4bd0e6fb624527668c438
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-23 11:17:25 +0200

    Implement cors and body parse in app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,11 @@
 import express from 'express'
 import { doctorsRoutes } from './routes/doctors.js'
+import bodyParser from 'body-parser'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
 doctorsRoutes(app)
 
 app.get('/', (req, res) => {
