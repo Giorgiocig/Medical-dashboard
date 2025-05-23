143e59868ebc62b4c73c9e307d31b29a44a94f2d
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-23 17:36:28 +0200

    Import DoctorCard component

---
+++ frontend/src/components/DoctorCard.tsx
@@ -0,0 +1,41 @@
+import Box from '@mui/material/Box'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import Typography from '@mui/material/Typography'
+
+export default function DoctorCard({
+    name,
+    surname,
+    speciality,
+    availableForOperatingRoom,
+    availableForClinic,
+}: {
+    name: string
+    surname: string
+    speciality: string
+    availableForOperatingRoom: boolean
+    availableForClinic: boolean
+}) {
+    return (
+        <Box sx={{ minWidth: 275 }}>
+            <Card variant='outlined'>
+                <CardContent>
+                    <Typography
+                        gutterBottom
+                        sx={{ color: 'text.secondary', fontSize: 14 }}
+                    >
+                        {name} {surname}
+                    </Typography>
+                    <Typography>{speciality}</Typography>
+                    <Typography>
+                        Available for operating room{' '}
+                        {availableForOperatingRoom ? 'YES' : 'NO'}
+                    </Typography>
+                    <Typography>
+                        Available for clinic {availableForClinic ? 'YES' : 'NO'}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Box>
+    )
+}
