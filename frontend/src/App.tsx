c8f36738492b5c16a3e643e8413e1874cd096cfd
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-26 16:55:02 +0200

    Embeed DoctorCardList, DoctorFilter, DoctorSorting, CreateDoctor components into App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,35 @@
+import { Typography } from '@mui/material'
+import CreateDoctor from './components/CreateDoctor'
+import DoctorCardList from './components/DoctorCardList'
+import DoctorFilter from './components/DoctorFilter'
+import DoctorSorting from './components/DoctorSorting'
+
+const doctors = [
+  {
+    name: 'gianni',
+    surname: 'brear',
+    speciality: 'ortopedia',
+    availableForOperatingRoom: true,
+    availableForClinic: false,
+  },
+  {
+    name: 'alfredo',
+    surname: 'nani',
+    speciality: 'medicina',
+    availableForOperatingRoom: false,
+    availableForClinic: false,
+  },
+]
+
 function App() {
-
-
   return (
-    <>
-      <p>hello</p>
-    </>
+    <div style={{ padding: 20 }}>
+      <DoctorCardList doctors={doctors} />
+      <DoctorFilter field="name" />
+      <DoctorSorting fields={["speciality"]} />
+      <Typography>CREATE DOCTOR</Typography>
+      <CreateDoctor />
+    </div>
   )
 }
 
