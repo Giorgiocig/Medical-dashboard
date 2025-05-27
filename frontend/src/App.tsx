221b12b524bd36ed4c3236fc4c7ad24323deadfe
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-27 10:19:47 +0200

    Modify App.tsx to embeed tanstack components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,13 @@
-import { Typography } from '@mui/material'
-import CreateDoctor from './components/CreateDoctor'
-import DoctorCardList from './components/DoctorCardList'
-import DoctorFilter from './components/DoctorFilter'
-import DoctorSorting from './components/DoctorSorting'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { Dashboard } from "./Dashboard"
 
-const doctors = [
-  {
-    name: 'gianni',
-    surname: 'brear',
-    speciality: 'ortopedia',
-    availableForOperatingRoom: true,
-    availableForClinic: false,
-  },
-  {
-    name: 'alfredo',
-    surname: 'nani',
-    speciality: 'medicina',
-    availableForOperatingRoom: false,
-    availableForClinic: false,
-  },
-]
+const queryClient = new QueryClient
 
-function App() {
-  return (
-    <div style={{ padding: 20 }}>
-      <DoctorCardList doctors={doctors} />
-      <DoctorFilter field="name" />
-      <DoctorSorting fields={["speciality"]} />
-      <Typography>CREATE DOCTOR</Typography>
-      <CreateDoctor />
-    </div>
-  )
+
+export default function App() {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Dashboard />
+        </QueryClientProvider>
+    )
 }
-
-export default App
