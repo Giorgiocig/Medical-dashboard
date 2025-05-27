95106c9ee48ff7b85c7e273e767b15be211b4118
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-27 15:06:24 +0200

    Implement logic to do POST request in CreateDoctor .tsx component (frontend)

--- frontend/src/components/CreateDoctor.tsx
+++ frontend/src/components/CreateDoctor.tsx
@@ -4,53 +4,118 @@ import RadioGroup from '@mui/material/RadioGroup'
 import FormControlLabel from '@mui/material/FormControlLabel'
 import FormControl from '@mui/material/FormControl'
 import FormLabel from '@mui/material/FormLabel'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { createDoctor } from '../api/doctors'
+import { useState } from 'react'
 
 export default function CreateDoctor({ }: {}) {
+    const [name, setName] = useState<string>('')
+    const [surname, setSurname] = useState<string>('')
+    const [speciality, setSpeciality] = useState<string>('')
+    const [availableForOperatingRoom, setAvailableForOperatingRoom] =
+        useState<boolean>(false)
+    const [availableForClinic, setAvailableForClinic] = useState<boolean>(false)
+    const queryClient = useQueryClient()
+
+    const createDoctorMutation = useMutation({
+        mutationFn: () =>
+            createDoctor({
+                name,
+                surname,
+                speciality,
+                availableForOperatingRoom,
+                availableForClinic,
+            }),
+        onSuccess: () => queryClient.invalidateQueries({ queryKey: ['doctors'] }),
+        onError: (error) => {
+            console.error('Doctor creation failed:', error)
+        },
+    })
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        console.log('Creating doctor with:')
+        e.preventDefault()
+        createDoctorMutation.mutate()
+    }
+
     return (
-        <div>
+        <form onSubmit={handleSubmit}>
             <FormControl>
                 <FormLabel id='demo-radio-buttons-group-label'>
                     Insert new doctor
                 </FormLabel>
-                <TextField id='outlined-basic' label='Name' variant='outlined' />
-                <TextField id='outlined-basic' label='Surname' variant='outlined' />
-                <TextField id='outlined-basic' label='Speciality' variant='outlined' />
+                <TextField
+                    id='outlined-basic'
+                    label='Name'
+                    variant='outlined'
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+                <TextField
+                    id='outlined-basic'
+                    label='Surname'
+                    variant='outlined'
+                    value={surname}
+                    onChange={(e) => setSurname(e.target.value)}
+                />
+                <TextField
+                    id='outlined-basic'
+                    label='Speciality'
+                    variant='outlined'
+                    value={speciality}
+                    onChange={(e) => setSpeciality(e.target.value)}
+                />
                 <Typography>available for operating room</Typography>
                 <RadioGroup
-                    aria-labelledby='"available for operating room"'
-                    defaultValue={true}
                     name='radio-buttons-group'
+                    value={availableForOperatingRoom.toString()}
+                    onChange={(e) =>
+                        setAvailableForOperatingRoom(e.target.value === 'true')
+                    }
                 >
                     <FormControlLabel
-                        value={true}
+                        value='true'
                         control={<Radio />}
-                        label='available'
+                        label='Available'
                     />
                     <FormControlLabel
-                        value={false}
+                        value='false'
                         control={<Radio />}
-                        label='not available'
+                        label='Not Available'
                     />
                 </RadioGroup>
-                <Typography>available for operating room</Typography>
+                <Typography>available for clinic </Typography>
                 <RadioGroup
                     aria-labelledby='"available for clinic"'
-                    defaultValue={true}
+                    value={availableForClinic.toString()}
                     name='radio-buttons-group'
+                    onChange={(e) => setAvailableForClinic(e.target.value === 'true')}
                 >
                     <FormControlLabel
-                        value={true}
+                        value='true'
                         control={<Radio />}
-                        label='available'
+                        label='Available'
                     />
                     <FormControlLabel
-                        value={false}
+                        value='false'
                         control={<Radio />}
-                        label='not available'
+                        label='Not Available'
                     />
                 </RadioGroup>
-                <Button variant="contained">Create</Button>
+                <Button
+                    type='submit'
+                    variant='contained'
+                    value={createDoctorMutation.isPending ? 'creating...' : 'Create'}
+                    disabled={
+                        !name || !surname || !speciality || createDoctorMutation.isPending
+                    }
+                >
+                    Create
+                </Button>
             </FormControl>
-        </div>
+            {createDoctorMutation.isSuccess && (
+                <Typography>Doctor create successfully</Typography>
+            )}
+        </form>
     )
 }
