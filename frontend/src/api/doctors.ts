c2261ac54c2712c8c3aa26c1caa31d89c7236a4f
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-27 15:05:53 +0200

    Add createDoctor function to POST request (frontend)

--- frontend/src/api/doctors.ts
+++ frontend/src/api/doctors.ts
@@ -1,7 +1,20 @@
+import type { IDoctor } from '../utilities/interfaces'
+
 export const getDoctors = async (queryParams: { [key: string]: any }) => {
   const res = await fetch(
     `${import.meta.env.VITE_BACKEND_URL}/doctors?` +
       new URLSearchParams(queryParams),
   )
+  return await res.json()
+}
+
+export const createDoctor = async (doctor: IDoctor) => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/doctors`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(doctor),
+  })
   return await res.json()
 }
