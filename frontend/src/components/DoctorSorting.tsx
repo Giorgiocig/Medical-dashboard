28494c59ef097ef754f8513f864fb83963015d07
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-27 10:20:37 +0200

    Add props to DoctorSorting component

--- frontend/src/components/DoctorSorting.tsx
+++ frontend/src/components/DoctorSorting.tsx
@@ -5,16 +5,29 @@ import FormControl from '@mui/material/FormControl'
 import Select from '@mui/material/Select'
 import { Typography } from '@mui/material'
 
-export default function DoctorSorting({ fields = [] }: { fields: string[] }) {
+export default function DoctorSorting({
+    fields = [],
+    value,
+    onChange,
+    orderValue,
+    onOrderChange,
+}: {
+    fields: string[]
+    value: string
+    onChange: (value: string) => void
+    orderValue: string
+    onOrderChange: (value: string) => void
+}) {
+    console.log(value)
     return (
         <Box display='flex' alignItems='center' gap={2}>
             <FormControl variant='outlined' size='small'>
-                <InputLabel id='sort-by-label'>Sort By</InputLabel>
+                <InputLabel id='sort-by-label'>Sort By Name</InputLabel>
                 <Select
                     labelId='sort-by-label'
                     id='sortBy'
-                    // value={value}
-
+                    value={value}
+                    onChange={(e) => onChange(e.target.value)}
                     label='Sort By'
                 >
                     {fields.map((field) => (
@@ -28,11 +41,12 @@ export default function DoctorSorting({ fields = [] }: { fields: string[] }) {
             <Typography>/</Typography>
 
             <FormControl variant='outlined' size='small'>
-                <InputLabel id='sort-order-label'>Sort Order</InputLabel>
+                <InputLabel id='sort-order-label'>Sort Order Name</InputLabel>
                 <Select
                     labelId='sort-order-label'
                     id='sortOrder'
-                    // value={orderValue}
+                    value={orderValue}
+                    onChange={(e) => onOrderChange(e.target.value)}
                     label='Sort Order'
                 >
                     <MenuItem value='ascending'>ascending</MenuItem>
