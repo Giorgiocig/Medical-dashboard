781151fa3f9a974d8b5b4cdb296086da67c8e8c6
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-27 11:26:57 +0200

    Embeed SpecialityFilter.tsx component in Dashboard

--- frontend/src/Dashboard.tsx
+++ frontend/src/Dashboard.tsx
@@ -5,20 +5,36 @@ import DoctorFilter from './components/DoctorFilter'
 import DoctorSorting from './components/DoctorSorting'
 import { useQuery } from '@tanstack/react-query'
 import { getDoctors } from './api/doctors'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import SpecialityFilter from './components/SpecialityFilter'
+import type { IDoctor } from './utilities/interfaces'
 
 export function Dashboard() {
   const [name, setName] = useState('')
   const [speciality, setSpeciality] = useState('')
   const [sortBy, setSortBy] = useState('speciality')
   const [sortOrder, setSortOrder] = useState('descending')
+  const [allSpecialities, setAllSpecialities] = useState<string[]>([])
 
   const doctorsQuery = useQuery({
     queryKey: ['doctors', { name, speciality, sortBy, sortOrder }],
-    queryFn: () => getDoctors({ name, sortBy, sortOrder }),
+    queryFn: () => getDoctors({ name, speciality, sortBy, sortOrder }),
   })
-  console.log(doctorsQuery.error)
   const doctors = doctorsQuery.data ?? []
+
+  useEffect(() => {
+    const fetchSpecialities = async () => {
+      try {
+        const doctors = await getDoctors({})
+        const specialities = (doctors.map((doctor: IDoctor) => doctor.speciality)
+        ).sort()
+        setAllSpecialities(specialities)
+      } catch (error) {
+        console.error('Errore nel fetch delle specialità:', error)
+      }
+    }
+    fetchSpecialities()
+  }, [])
 
   return (
     <div style={{ padding: 20 }}>
@@ -42,6 +58,10 @@ export function Dashboard() {
         orderValue={sortOrder}
         onOrderChange={(orderValue) => setSortOrder(orderValue)}
       />
+      <Typography>Speciality Filter</Typography>
+      <SpecialityFilter value={speciality}
+        onChange={(value) => setSpeciality(value)}
+        options={allSpecialities} />
       <Typography>CREATE DOCTOR</Typography>
       <CreateDoctor />
     </div>
