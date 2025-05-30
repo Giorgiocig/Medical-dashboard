0da6e0c5b5aa72af46926a63c1590ef066a10036
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-05-30 14:27:57 +0200

    Implement DoctorPage.tsx component

---
+++ frontend/src/components/doctorPage.tsx/DoctorPage.tsx
@@ -0,0 +1,81 @@
+import { Box, Typography } from '@mui/material'
+import DoctorCardList from './DoctorCardList'
+import DoctorFilter from './DoctorFilter'
+import DoctorSorting from './DoctorSorting'
+import SpecialityFilter from './SpecialityFilter'
+import CreateDoctor from './CreateDoctor'
+import { useEffect, useState } from 'react'
+import useDebounce from '../../hooks/useDebounce'
+import { useQuery } from '@tanstack/react-query'
+import { getDoctors } from '../../api/doctors'
+import type { IDoctor } from '../../utilities/interfaces'
+
+type Props = {}
+
+export default function DoctorPage({ }: Props) {
+    const [name, setName] = useState('')
+    const [speciality, setSpeciality] = useState('')
+    const [sortBy, setSortBy] = useState('speciality')
+    const [sortOrder, setSortOrder] = useState('descending')
+    const [allSpecialities, setAllSpecialities] = useState<string[]>([])
+    // debounce
+    const debouncedName = useDebounce(name, 500)
+
+    const doctorsQuery = useQuery({
+        queryKey: ['doctors', { name, speciality, sortBy, sortOrder }],
+        queryFn: () => getDoctors({ debouncedName, speciality, sortBy, sortOrder }),
+    })
+
+    const doctors = doctorsQuery.data ?? []
+
+    useEffect(() => {
+        const fetchSpecialities = async () => {
+            try {
+                const doctors = await getDoctors({})
+                const specialities = doctors
+                    .map((doctor: IDoctor) => doctor.speciality)
+                    .sort()
+                setAllSpecialities(specialities)
+            } catch (error) {
+                console.error('Errore nel fetch delle specialità:', error)
+            }
+        }
+        fetchSpecialities()
+    }, [doctors])
+
+    return (
+        <Box>
+            <Typography sx={{ paddingBottom: 3 }}>
+                Numbers of Doctors in the building: {doctors.length}
+            </Typography>
+            <DoctorCardList doctors={doctors} />
+            <Typography>NAME</Typography>
+            <DoctorFilter
+                field='name'
+                value={name}
+                onChange={(value) => setName(value)}
+            />
+            {/* <Typography>SPECIALITY</Typography>
+        <DoctorFilter
+          field='speciality'
+          value={speciality}
+          onChange={(value) => setSpeciality(value)}
+        /> */}
+            <DoctorSorting
+                fields={['speciality']}
+                value={sortBy}
+                onChange={(value) => setSortBy(value)}
+                orderValue={sortOrder}
+                onOrderChange={(orderValue) => setSortOrder(orderValue)}
+            />
+            <Typography>Speciality Filter</Typography>
+            <SpecialityFilter
+                value={speciality}
+                onChange={(value) => setSpeciality(value)}
+                options={allSpecialities}
+            />
+            <Typography>CREATE DOCTOR</Typography>
+            <CreateDoctor />
+        </Box>
+    )
+}
