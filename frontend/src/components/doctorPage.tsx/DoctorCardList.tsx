63074077724c460035e655e48524d20a4c7c3119
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-06-03 15:08:48 +0200

    Cast interface to DoctorCardList

--- frontend/src/components/doctorPage.tsx/DoctorCardList.tsx
+++ frontend/src/components/doctorPage.tsx/DoctorCardList.tsx
@@ -2,7 +2,7 @@ import { Box } from '@mui/material'
 import type { IDoctor, IDoctorCardList } from '../../utilities/interfaces'
 import DoctorCard from './DoctorCard'
 
-export default function DoctorCardList({ doctors = [], handleClickOpen }: any) {
+export default function DoctorCardList({ doctors = [], handleClickOpen }: IDoctorCardList) {
   return (
     <Box sx={{ display: "flex", flexDirection: "column", gap: 3 }}>
       {doctors.map((doctor: IDoctor, index: number) => (
