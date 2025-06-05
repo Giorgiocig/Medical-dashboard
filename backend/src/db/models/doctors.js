3bc1b5e133f1911664c7c6d17d33b3eefea653a4
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-06-05 15:15:30 +0200

    Update schema to include email validation (backend)

--- backend/src/db/models/doctors.js
+++ backend/src/db/models/doctors.js
@@ -1,12 +1,16 @@
 import mongoose, { Schema } from 'mongoose'
 
 const doctorSchema = new Schema({
-  name: { type: String, required: true },
-  surname: { type: String, required: true },
+  name: { type: String, required: [true, '`name` is required'] },
+  surname: { type: String, required: [true, '`surname` is required'] },
   speciality: { type: String, required: true },
   availableForOperatingRoom: { type: Boolean, required: true },
   availableForClinic: { type: Boolean, required: true },
-  email: { type: String, required: true, unique: true },
+  email: {
+    type: String,
+    required: true,
+    match: [/@/, "Not valid email.'@' is missing"],
+  },
 })
 
 export const Doctor = mongoose.model('doctor', doctorSchema)
