4a62c4d6f3777d5fef65751ff4afbcca2b1d686c
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-06-11 16:42:28 +0200

    Refactor VerticalBarCharts embeed graphData and graphOptions helper functions

--- frontend/src/components/doctorPage.tsx/VerticalBarsChart.tsx
+++ frontend/src/components/doctorPage.tsx/VerticalBarsChart.tsx
@@ -9,69 +9,32 @@ import {
 } from 'chart.js'
 
 import { Bar } from 'react-chartjs-2'
-import type { IDoctor } from '../../utilities/interfaces'
 import { Box } from '@mui/material'
+
+import { graphData, graphOptions } from '../../utilities/helpers'
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
 
-export default function VerticalBarChart({ sampleData }: { sampleData: any }) {
+export default function VerticalBarChart({
+    text,
+    sampleData,
+}: {
+    text: string
+    sampleData: { [key: string]: number }
+}) {
     let xAxisLabels: string[] = []
-    let dataValues: any = []
+    let dataValues: number[] = []
 
     for (const [key, value] of Object.entries(sampleData)) {
         xAxisLabels.push(key)
         dataValues.push(value)
     }
 
-    const options = {
-        responsive: true,
-        plugins: {
-            legend: {
-                display: false,
-            },
-            title: {
-                display: true,
-                text: 'Specialities',
-            },
-        },
-        scales: {
-            y: {
-                beginAtZero: true,
-            },
-        },
-    }
-
-    const data = {
-        labels: xAxisLabels,
-        datasets: [
-            {
-                data: dataValues,
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.6)',
-                    'rgba(54, 162, 235, 0.6)',
-                    'rgba(255, 206, 86, 0.6)',
-                    'rgba(75, 192, 192, 0.6)',
-                    'rgba(153, 102, 255, 0.6)',
-                    'rgba(255, 159, 64, 0.6)',
-                    'rgba(199, 199, 199, 0.6)',
-                ],
-                borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)',
-                    'rgba(199, 199, 199, 1)',
-                ],
-                borderWidth: 1,
-
-            },
-        ],
-    }
+    const data = graphData(xAxisLabels, dataValues)
+    const options = graphOptions(text)
 
     return (
-        <Box style={{ width: '600px', height: '400px' }}>
+        <Box style={{ width: '500px' }}>
             <Bar data={data} options={options} />
         </Box>
     )
