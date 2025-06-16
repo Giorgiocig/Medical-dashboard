786f74e7f162b03aabf836bfa161f2557dfa3c5d
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-06-16 15:52:13 +0200

    Register specialities routes in app.js (backend)

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,12 +2,13 @@ import express from 'express'
 import { doctorsRoutes } from './routes/doctors.js'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import { specialitiesRoutes } from './routes/specialities.js'
 
 const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 doctorsRoutes(app)
-
+specialitiesRoutes(app)
 app.get('/', (req, res) => {
   res.send('Hello from express')
 })
