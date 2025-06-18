a585fb6c7743d5f55ab7d0b8d3afc39f768cdd3d
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-06-18 17:23:27 +0200

    Implement api  fns to get and create specialities

---
+++ frontend/src/api/specialities.ts
@@ -0,0 +1,21 @@
+export const getSpecialities = async () => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/specialities`)
+  return await res.json()
+}
+
+export const createSpeciality = async (specialityName: string) => {
+  const res = await fetch(`${import.meta.env.VITE_BACKEND_URL}/specialities`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ specialityName }),
+  })
+
+  if (!res.ok) {
+    const errorBody = await res.json()
+    throw new Error(errorBody.message || 'Something went wrong')
+  }
+
+  return await res.json()
+}
