9eb95a4d86b28ffd0b30e11b1667f319c6628ac0
gcignitti <giorgio.cignitti@cenotelie.fr>
2025-06-18 17:21:51 +0200

    Add DOCTOR_FILTER_SECTION_OPTIONS

--- frontend/src/utilities/constants.ts
+++ frontend/src/utilities/constants.ts
@@ -7,3 +7,9 @@ export const BACKGROUND_COLUMN_GRAPH_COLORS = [
   'rgba(255, 159, 64, 0.6)',
   'rgba(199, 199, 199, 0.6)',
 ]
+
+export const DOCTOR_FILTER_SECTION_OPTIONS = [
+  { title: '', field: '', value: '', onChange: '' },
+  { fields: '', value: '', onChange: '', orderValue: '', onOrderChange: '' },
+  { value: '', onChange: '', doctors: '' },
+]
